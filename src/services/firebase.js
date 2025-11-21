579be0100d2b4f2482242113ba423f64948b7310
CHAD_mulligan <itskenedy5@gmail.com>
2025-11-21 04:09:52 +0300

    updated dashboard reduced writes temp db

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -6,25 +6,14 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 // Your AI-TRAINING-TASKS DB
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAp5CrpSIbVL3AFl-cfBHBBDE8xpDwvbTA",
-//   authDomain: "ai-training-tasks.firebaseapp.com",
-//   projectId: "ai-training-tasks",
-//   storageBucket: "ai-training-tasks.firebasestorage.app",
-//   messagingSenderId: "635815062881",
-//   appId: "1:635815062881:web:3e0463099cda2b712fa920",
-//   measurementId: "G-Q8QG41159X"
-// };
-
-// Your OUTLIER DB
-const firebaseConfig = {
-   apiKey: "AIzaSyA0uNerHR8ydHhkA6rrq1gUJXZ9Dd_uSVs",
-   authDomain: "outlier-tasks-a3b50.firebaseapp.com",
-   projectId: "outlier-tasks-a3b50",
-   storageBucket: "outlier-tasks-a3b50.firebasestorage.app",
-   messagingSenderId: "423623709538",
-   appId: "1:423623709538:web:2db34260f843dad5a6923b",
-   measurementId: "G-9RK5Q61P1B"
+ const firebaseConfig = {
+ apiKey: "AIzaSyAp5CrpSIbVL3AFl-cfBHBBDE8xpDwvbTA",
+   authDomain: "ai-training-tasks.firebaseapp.com",
+   projectId: "ai-training-tasks",
+   storageBucket: "ai-training-tasks.firebasestorage.app",
+   messagingSenderId: "635815062881",
+   appId: "1:635815062881:web:3e0463099cda2b712fa920",
+   measurementId: "G-Q8QG41159X"
  };
 
 
