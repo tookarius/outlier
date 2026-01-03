b536ba583198aad6c35902cb6ab066bf5be7957e
TK <121897808+tookarius@users.noreply.github.com>
2026-01-03 20:05:20 +0300

    Update stk-push.js

--- api/stk-push.js
+++ api/stk-push.js
@@ -58,7 +58,7 @@ module.exports = async (req, res) => {
     const payload = {
       amount: Number(amount),
       phone_number: formattedPhone,
-      channel_id: 4669,
+      channel_id: 4832,
       provider: 'm-pesa',
       external_reference: reference,
       callback_url: callbackUrl,
